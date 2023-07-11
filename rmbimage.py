acf41a50df1f60a813e17aa3c4e24361870c80b1
robertbyers1111 <robertbyers1111@gmail.com>
2023-07-11 08:52:09 -0400

    Added new methods add_filename_to_image and image_sequence_to_mp4 [rmbimage.py]

--- rmbimage.py
+++ rmbimage.py
@@ -5,16 +5,19 @@ rmbimage.py - My image library
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 """
 
-import PIL as Image  # <-- for future use
 import subprocess as sp
 import rmblogging
 from rmblogging import debug, error
+from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw
+from moviepy.editor import ImageSequenceClip
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 def run_gegl(gegl_params):
-"""
-Invoke the external gegl CLI process using the command line passed in from caller
-"""
+    """
+    Invoke the external gegl CLI process using the command line passed in from caller
+    """
 
     gegl_full_command = ['gegl'] + gegl_params
 
@@ -40,4 +43,44 @@ Invoke the external gegl CLI process using the command line passed in from calle
 
     for line in completed_process.stdout:
         debug(f"{line[:188] = }")
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+def add_filename_to_image(input_filename, output_filename):
+    """
+    Writes the name of an image file in the top left corner of the image, using a small font.
+    """
+
+    debug(f'Adding filename {output_filename} to the top left corner..')
+
+    x = y = 0
+    font_name = 'kalimati.ttf'
+    font_size = 8 
+    color = (255, 255, 255)
+
+    debug(f'{font_name = }')
+    debug(f'{font_size = }')
+    debug(f'{color = }')
+
+    with Image.open(input_filename) as img:
+        font = ImageFont.truetype(font_name, font_size)
+        draw = ImageDraw.Draw(img)
+        draw.text((x, y), f"[{output_filename}]", color, font=font)
+        img.save(output_filename)
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+def image_sequence_to_mp4(clips_directory, fps, output_filename):
+    """
+    Uses moviepy.ImageSequenceClip to generate a single mp4 file from a sequence of images.
+
+    Notes..
+
+    • All images must be in a single directory.
+    • There must not be anything other than the image sequence files in the sequence directory.
+    • All images must be of the same dimension.
+
+    """
+    clip = ImageSequenceClip(clips_directory, fps=fps)
+    clip.write_videofile(output_filename)
 
